1328362ec241f77d6a35a599d85979794e9086f5
Bermeo Ortiz Juan Diego (jberme) <jberme@login0>
2023-01-02 18:13:20 +0100

    ran tunning jobs polish

---
+++ compute.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#SBATCH --output=tunning_jobs/po_rf_%j.out
+#SBATCH --time=36:10:00
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=61600
+module load generic
+module load anaconda3
+source activate Framing
+srun python benchmark_subtask_2.py --config_path_yaml config.yaml
