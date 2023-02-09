1a098d839e58a1d3a3d8b8d39eeebefe49ff3470
Juan Bermeo <juandiego.bermeoortiz@uzh.ch>
2023-02-09 17:57:43 +0100

    testing warmup

--- llm_benchmark.sh
+++ llm_benchmark.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-#SBATCH --output=llm_benchmark_jobs/%2j_xlm_roberta_512_minib_4_gradsteps_4.out
+#SBATCH --output=llm_benchmark_jobs/%2j_xlm_roberta_512_minib_4_gradsteps_2_warmup_0.1.out
 #SBATCH --ntasks=1
 #SBATCH --time=04:00:00
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=8GB
+#SBATCH --mem=16GB
 #SBATCH --gres gpu:1
 module load gpu
 module load mamba
 source activate Framing_HF_2
 srun python benchmark_llms.py \
   --config_path_yaml config_llm_benchmark.yaml\
-  --gradient_accumulation_steps 4\
+  --gradient_accumulation_steps 2\
   --max_length_padding 512\
   --minibatch_size 4
