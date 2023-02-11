742307de84568a6945c8e2bb6f02b24386f6c87c
Juan Bermeo <juandiego.bermeoortiz@uzh.ch>
2023-02-11 15:50:10 +0100

    last job runned

--- llm_benchmark.sh
+++ llm_benchmark.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH --output=llm_benchmark_jobs/%2j_xlm_roberta_512_minib_4_gradsteps_2_warmup_0.2_data_collator.out
+#SBATCH --output=llm_benchmark_jobs/%2j_mDistillbert_512_minib_16_gradsteps_2_warmup_0.2_data_collator_title_test.out
 #SBATCH --ntasks=1
 #SBATCH --time=04:00:00
 #SBATCH --cpus-per-task=4
@@ -7,9 +7,14 @@
 #SBATCH --gres gpu:1
 module load gpu
 module load mamba
+cat llm_benchmark.sh
+cat config_llm_benchmark.yaml
 source activate Framing_HF_2
 srun python benchmark_llms.py \
   --config_path_yaml config_llm_benchmark.yaml\
+  --model_name distilbert-base-multilingual-cased\
   --gradient_accumulation_steps 2\
   --max_length_padding 512\
-  --minibatch_size 4
+  --minibatch_size 2056\
+  --n_epochs 1\
+  --analysis_unit title
