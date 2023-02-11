9e0278df52123a72db4130e96da88af7c4d8cb65
JdbermeoUZH <juandiego.bermeoortiz@uzh.ch>
2023-02-11 18:49:29 +0100

    added python script to submit multiple slurm jobs

--- modifiable_llm_benchmark.sh
+++ modifiable_llm_benchmark.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
-#SBATCH --output=llm_benchmark_jobs/%2j_$model_name_minib_$minibatch_size_gradsteps_$gradient_accumulation_steps_warmup_0.2_data_collator_$analysis_unit.out
+#SBATCH --output=llm_benchmark_jobs/%2j_${model_name}_minib_${minibatch_size}_gradsteps_${gradient_accumulation_steps}_warmup_0.2_data_collator_$analysis_unit.out
 #SBATCH --ntasks=1
-#SBATCH --time=04:00:00
+#SBATCH --time=05:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16GB
 #SBATCH --gres gpu:1
+
 module load gpu
 module load mamba
 cat llm_benchmark.sh
 cat config_llm_benchmark.yaml
 source activate Framing_HF_2
+
 srun python benchmark_llms.py \
   --config_path_yaml config_llm_benchmark.yaml\
   --model_name $model_name\
