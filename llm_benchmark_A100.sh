285cb4031a6823c9bfcc6933be68b79b0dbb5709
Juan Bermeo <juandiego.bermeoortiz@uzh.ch>
2023-02-12 15:50:21 +0100

    tested parameters for larger models

--- llm_benchmark_A100.sh
+++ llm_benchmark_A100.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-#SBATCH --output=llm_jobs/%2j_GPT-NEO-1.3B.out
+#SBATCH --output=llm_jobs/%2j_gptJ_8bit.out
 #SBATCH --ntasks=1
 #SBATCH --time=04:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64GB
-#SBATCH --constraint=GPUMEM32GB
-#SBATCH --gres=gpu:V100:1 
+#SBATCH --constraint=GPUMEM80GB
+#SBATCH --gres=gpu:A100:1 
 
 module load gpu
 module load mamba
 
-cat llm_benchmark.sh
+cat llm_benchmark_A100.sh
 cat config_llm_benchmark.yaml
 
 source activate Framing_HF_2
@@ -18,9 +18,9 @@ nvidia-smi
 
 srun python benchmark_llms.py \
   --config_path_yaml config_llm_benchmark.yaml\
-  --model_name EleutherAI/gpt-neo-1.3B\
+  --model_name EleutherAI/gpt-j-6B\
   --gradient_accumulation_steps 2\
   --max_length_padding 512\
-  --minibatch_size 1\
+  --minibatch_size 2\
   --n_epochs 1\
   --analysis_unit raw_text
