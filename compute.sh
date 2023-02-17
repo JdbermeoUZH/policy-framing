39f33de0a816e89dfc4b96d45585e3237ea27dbd
JdbermeoUZH <juandiego.bermeoortiz@uzh.ch>
2023-02-17 17:51:35 +0100

    ready to run in cluster

--- compute.sh
+++ compute.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH --output=benchmark_jobs/%2j_po_benchmark_1st_sent_each_p_fixed.out
+#SBATCH --output=benchmark_jobs/%2j_default_params.out
 #SBATCH --time=43:59:59
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=61600
@@ -7,10 +7,5 @@ module load mamba
 source activate Framing_py39
 srun python benchmark_subtask_2.py \
   --config_path_yaml config.yaml\
-  --tune_preprocessing_params 1\
-  --analysis_unit raw_text title\
-  --n_samples 2\
-  --experiment_base_name test_cmd_line_args\
-  --model_list LogisticRegression LogisticRegression\
   --mlb_cls_independent 0\
   --default_params 1
