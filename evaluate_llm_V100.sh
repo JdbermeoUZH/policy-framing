1b104b20b6c73dc025527d173e070cc16796d38b
JdbermeoUZH <juandiego.bermeoortiz@uzh.ch>
2023-02-27 18:56:45 +0100

    script to evaluate llms on 80/20 split

---
+++ evaluate_llm_V100.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#SBATCH --output=llm_jobs/%2j.out
+#SBATCH --ntasks=1
+#SBATCH --time=08:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64GB
+#SBATCH --constraint=GPUMEM32GB
+#SBATCH --gres=gpu:V100:1
+
+module load gpu
+module load mamba
+cat modifiable_llm_benchmark.sh
+cat config_llm_benchmark.yaml
+source activate Framing_HF_2
+
+srun python tune_and_evaluate_llms.py \
+  --config_path_yaml config_llm_final_evaluation.yaml\
+  --model_name $model_name\
+  --gradient_accumulation_steps $gradient_accumulation_steps\
+  --max_length_padding $max_length_padding\
+  --minibatch_size $minibatch_size\
+  --n_epochs $n_epochs\
+  --analysis_unit $analysis_unit\
+  --truncated $truncated\
+  --single_train_test_split_filepath $single_train_test_split_filepath
---
+++ run_llm_compute_final_report.py
@@ -0,0 +1,111 @@
+import os
+
+from utils.constants import UNITS_OF_ANALYSES
+
+
+LLMS = {
+    'xlm-roberta-large': {
+        'gradient_accumulation_steps': 2,
+        'minibatch_size': 4,
+        'n_epochs': 7,
+        'max_length_padding': 512,
+        'run_on': 'GPUMEM16GB'
+    },
+
+    'bert-base-multilingual-cased': {
+        'gradient_accumulation_steps': 2,
+        'minibatch_size': 16,
+        'n_epochs': 65,
+        'max_length_padding': 512,
+        'run_on': 'GPUMEM16GB'
+    },
+
+    'distilbert-base-multilingual-cased': {
+        'gradient_accumulation_steps': 1,
+        'minibatch_size': 64,
+        'n_epochs': 75,
+        'max_length_padding': 512,
+         'run_on': 'GPUMEM16GB'
+    },
+
+    'gpt2': {
+        'gradient_accumulation_steps': 2,
+        'minibatch_size': 4,
+        'n_epochs': 15,
+        'max_length_padding': 512,
+        'run_on': 'GPUMEM16GB'
+    },
+
+    'EleutherAI/gpt-neo-125M': {
+        'gradient_accumulation_steps': 4,
+        'minibatch_size': 2,
+        'n_epochs': 13,
+        'max_length_padding': 512,
+        'run_on': 'GPUMEM16GB'
+    },
+
+    'AshtonIsNotHere/xlm-roberta-long-base-4096': {
+        'gradient_accumulation_steps': 2,
+        'minibatch_size': 4,
+        'n_epochs': 20,
+        'max_length_padding': 4096,
+        'run_on': 'GPUMEM32GB'
+    },
+
+    'facebook/mbart-large-50': {
+        'gradient_accumulation_steps': 2,
+        'minibatch_size': 4,
+        'n_epochs': 7,
+        'max_length_padding': 512,
+        'run_on': 'GPUMEM32GB'
+    },
+
+    'EleutherAI/gpt-neo-1.3B': {
+        'gradient_accumulation_steps': 4,
+        'minibatch_size': 2,
+        'n_epochs': 8,
+        'max_length_padding': 512,
+        'run_on': 'GPUMEM80GB'
+    },
+}
+
+
+if __name__ == '__main__':
+    truncated = 1
+
+    for model_name, model_params in LLMS.items():
+        print(model_name)
+        print(model_params)
+        for analysis_unit in UNITS_OF_ANALYSES:
+            os.environ['analysis_unit'] = analysis_unit
+            os.environ['model_name'] = model_name
+            os.environ['gradient_accumulation_steps'] = str(model_params['gradient_accumulation_steps'])
+            os.environ['minibatch_size'] = str(model_params['minibatch_size'])
+            os.environ['n_epochs'] = str(model_params['n_epochs'])
+            os.environ['max_length_padding'] = str(model_params['max_length_padding'])
+
+            os.environ['truncated'] = str(truncated)
+
+            if truncated == 0:
+                os.environ['single_train_test_split_filepath'] = f'multilingual_train_test_80_20_split_{analysis_unit}_' \
+                                                                 f'max_words_length_500_min_words_length_30_chunk_word_overlap_250.hf'
+            elif truncated == 1:
+                os.environ['single_train_test_split_filepath'] = 'multilingual_train_test_ds_80_20_split.hf'
+
+            else:
+                raise RuntimeError('truncated must be 0 or 1')
+
+            print("run slurm job")
+
+            if model_params['run_on'] == 'GPUMEM16GB':
+                os.system('sbatch evaluate_llm.sh')
+
+            elif model_params['run_on'] == 'GPUMEM32GB':
+                os.system('sbatch evaluate_llm_V100.sh')
+
+            elif model_params['run_on'] == 'GPUMEM80GB':
+                os.system('sbatch evaluate_llm_A100.sh')
+
+            else:
+                assert False
+
